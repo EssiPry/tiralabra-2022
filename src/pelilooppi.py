a0f175e6055245bec4b8ec8892afa1a12f0794a6
Essi Prykäri <essi.prykari@helsinki.fi>
2022-05-05 12:19:06 +0300

    lisää kloonauksen pelilooppiin

--- src/pelilooppi.py
+++ src/pelilooppi.py
@@ -41,10 +41,12 @@ class Pelilooppi:
                 for koordinaatit in mahdolliset_siirrot:
                     self.ristinolla.pelilauta[koordinaatit[0]
                                           ][koordinaatit[1]] = 'X'
-                    mahdolliset_siirrot.remove(koordinaatit)
+                    kloonisiirrot = set(mahdolliset_siirrot)
+                    kloonisiirrot.remove(koordinaatit)
+                    self.ristinolla.paivita_mahdolliset_siirrot(koordinaatit, kloonisiirrot)
                     siirron_arvo = self.botti.minimax_ab(
-                    self.ristinolla, 3, -100, 100, False, koordinaatit, mahdolliset_siirrot)
-                    mahdolliset_siirrot.add(koordinaatit)
+                    self.ristinolla, 3, -100, 100, False, koordinaatit, kloonisiirrot)
+                    kloonisiirrot.add(koordinaatit)
                     print(koordinaatit, 'siirron arvo', siirron_arvo)
                     if siirron_arvo > arvo:
                         arvo = siirron_arvo
